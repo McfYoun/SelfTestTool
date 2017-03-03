1a50e950961f17f549d8e853d6aa6414456466cc
ted.jiang <ted.jiang@b-padynamics.com>
2017-03-03 10:26:12 +0800

    Initial Commit

---
+++ TCP_IP debug/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TCP_IP debug
+//
+//  Created by BP on 03/03/2017.
+//  Copyright © 2017 B&P. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ TCP_IP debug/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  TCP_IP debug
+//
+//  Created by BP on 03/03/2017.
+//  Copyright © 2017 B&P. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+    // Do any additional setup after loading the view.
+}
+
+
+- (void)setRepresentedObject:(id)representedObject {
+    [super setRepresentedObject:representedObject];
+
+    // Update the view, if already loaded.
+}
+
+
+@end
